61d5f29e57a90dd41bb5ec6dd813c7b4625d8602
Natalie Ike <nataliesenger@Natalies-MBP.fios-router.home>
2017-11-09 00:20:19 -0800

    build

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Add routes, both API and view
+app.use(express.static("client/build"));
+
 app.use(routes);
 
 // Set up promises with mongoose
@@ -30,10 +32,10 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+/*app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
+*/
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
